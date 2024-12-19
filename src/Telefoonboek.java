913ffcfac12ffa749f150eed60cad0d3c8703175
Lars De Richter <lars.derichter@gmail.com>
2024-12-19 10:44:08 +0100

    Telefoonboek

---
+++ src/Telefoonboek.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+public class Telefoonboek {
+    HashMap<String, String> contacts = new HashMap<>();
+
+    public Telefoonboek() {
+        contacts.put("Jan", "123");
+        contacts.put("Jos", "456");
+        contacts.put("Jozef", "789");
+    }
+
+    public int telContacten() {
+        return contacts.size();
+    }
+
+    public String zoekTelefoonnummerOp(String naam) {
+        String telefoonnummer = contacts.get(naam);
+        if (telefoonnummer != null) {
+            return telefoonnummer;
+        }
+        else {
+            return "99999";
+        }
+    }
+
+    public void plaatsInBoek(String naam, String telefoonnummer) {
+        contacts.put(naam, telefoonnummer);
+    }
+
+    public boolean komtVoor(String naam){
+        return contacts.containsKey(naam);
+    }
+
+    public void printAlleNamen() {
+        for (String naam : contacts.keySet()) {
+            System.out.println(naam);
+        }
+    }
+
+    public void printAlles() {
+        for (String naam : contacts.keySet()) {
+            System.out.println(naam + ": " + contacts.get(naam));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Telefoonboek{" +
+                "contacts=" + contacts +
+                '}';
+    }
+}
---
+++ src/TestTelefoonboek.java
@@ -0,0 +1,24 @@
+public class TestTelefoonboek {
+    public static void main(String[] args) {
+        Telefoonboek telefoonboek = new Telefoonboek();
+
+        System.out.println("--- Telefoonboek ---" );
+        System.out.println(telefoonboek);
+        System.out.println("\n--- telContacten ---");
+        System.out.println(telefoonboek.telContacten());
+        System.out.println("\n--- zoekTelefoonnummerOp (Jan) ---");
+        System.out.println(telefoonboek.zoekTelefoonnummerOp("Jan"));
+        System.out.println("\n--- zoekTelefoonnummerOp (onbestaand) ---");
+        System.out.println(telefoonboek.zoekTelefoonnummerOp("onbestaand"));
+        System.out.println("\n--- plaatsInBoek ---");
+        telefoonboek.plaatsInBoek("Lars", "234");
+        System.out.println("Aantal nummers: " + telefoonboek.telContacten());
+        System.out.println("\n--- komtVoor ---");
+        System.out.println("Jan komt voor? " + telefoonboek.komtVoor("Jan"));
+        System.out.println("Onbekend komt voor? " + telefoonboek.komtVoor("Onbekend"));
+        System.out.println("\n--- printAlleNamen ---");
+        telefoonboek.printAlleNamen();
+        System.out.println("\n--- printAlles ---");
+        telefoonboek.printAlles();
+    }
+}
